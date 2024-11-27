98b72becf2575901c7965d25f21d4192a08bd736
Seu Nome <herculeseugenio@rede.ulbra.br>
2024-11-26 22:47:56 -0300

    version-1

--- src/MatematicaListenerCompleto.js
+++ src/MatematicaListenerCompleto.js
@@ -2,6 +2,7 @@ import antlr4 from 'antlr4';
 import MatematicaListener from './parser/MatematicaListener';
 import MatematicaParser from './parser/MatematicaParser';
 
+
 export default class MatematicaListenerCompleto extends MatematicaListener {
   constructor() {
     super();
